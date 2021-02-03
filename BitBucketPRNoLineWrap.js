b1c3d6a008e0647f945573c0dfc95a63ec9d0d51
radlab24 <50619499+radlab24@users.noreply.github.com>
2021-02-03 19:40:20 +0100

    Initial version fo BitBucket 7+

---
+++ BitBucketPRNoLineWrap.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         BitBucketPRNoLineWrap
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  codespace no line wrap in pullrequest diff
+// @author       RadLabSD
+// @updateSite   https://github.com/radlab24/Scripts/master/BitBucketPRNoLineWrap.js
+// @downloadURL  https://github.com/radlab24/Scripts/master/BitBucketPRNoLineWrap.js
+// @match        https://codespace.lhsystems.int/projects/LIDO_ERM/repos/erm-implementation/pull-requests/*/diff*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function addGlobalStyle(css) {
+        var head, style;
+        head = document.getElementsByTagName('head')[0];
+        if (!head) { return; }
+        style = document.createElement('style');
+        style.type = 'text/css';
+        style.innerHTML = css;
+        head.appendChild(style);
+    }
+    addGlobalStyle('td.diff-line { white-space: pre; }');
+})();
